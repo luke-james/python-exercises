f85be3ea2606bbc15463377f09ca1a2890b123e6
Luke James <lukejamesa19@gmail.com>
2021-01-30 16:59:47 +0000

    - Added some docstrings to explain the functions.

--- src/ex2_odd_or_even.py
+++ src/ex2_odd_or_even.py
@@ -1,5 +1,9 @@
 class OddsOrEvens:
 
+    '''
+    This class will calculate the response message for the input given.
+    '''
+
     def __init__(self, numbers=None):
 
         if numbers is None:
@@ -11,6 +15,8 @@ class OddsOrEvens:
 
     def calculate(self):
 
+        # This function will work out which function to call based on the number of inputs.
+
         if len(self.numbers) == 2:
             return self.__divides_whole(self.numbers[1], self.numbers[0])
 
@@ -20,18 +26,29 @@ class OddsOrEvens:
         raise ValueError
 
     def __is_number_odd(self, num):
+
+        # This function will work out whether the number to check is odd.
+
         message = self.even_message if num % 2 == 0 else self.odd_message
         return self.__is_number_multiple_of_4(num, message)
 
     @staticmethod
     def __is_number_multiple_of_4(num, message):
+
+        # This function will calculate whether or not the number to check is a multiple of 4.
+
         return message if not num % 4 == 0 else "The number is a multiple of 4!"
 
     def __divides_whole(self, num1, num2):
+
+        # This function will calculate whether or not the number to check divides wholly into into the number provided.
+
         return "The numbers divide exactly!" if num1 % num2 == 0 else self.__is_number_odd(num1)
 
 
 if __name__ == "__main__":
+
+    # Main function to run the program.
 
     while True:
 
